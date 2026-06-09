a8f094889cc5d32997ced79c1bd3030efbcff991
talha <tq74198@gmail.com>
2026-06-09 10:15:57 +0530

    build fix

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"ENN3S4mYwE7j/0KYezexGhcNDx6lJMuF/rQI2a6jxmo=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
